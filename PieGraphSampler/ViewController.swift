2b6254a893c02b049cf58537513d354cb907c024
Masashi Sutou <sutou.masasi@gmail.com>
2018-08-23 19:36:19 +0900

    Initial Commit

---
+++ PieGraphSampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PieGraphSampler
+//
+//  Created by 須藤将史 on 2018/08/23.
+//  Copyright © 2018年 須藤将史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PieGraphSamplerTests/PieGraphSamplerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PieGraphSamplerTests.swift
+//  PieGraphSamplerTests
+//
+//  Created by 須藤将史 on 2018/08/23.
+//  Copyright © 2018年 須藤将史. All rights reserved.
+//
+
+import XCTest
+@testable import PieGraphSampler
+
+class PieGraphSamplerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
